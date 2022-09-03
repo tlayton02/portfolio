f44541aea8d60a684e8ad8790c69b50147b5ec99
tlayton02 <47725456+tomlayton@users.noreply.github.com>
2022-09-03 19:54:09 +0100

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,20 @@
 var slider = function (sliderElement) {
-
   var pages = [];
   var currentSlide = 1;
   var isChanging = false;
-  var keyUp = {38:1, 33:1};
-  var keyDown = {40:1, 34:1};
+  var keyUp = { 38: 1, 33: 1 };
+  var keyDown = { 40: 1, 34: 1 };
 
   var init = function () {
-
-    document.body.classList.add('slider__body');
+    document.body.classList.add("slider__body");
 
     // control scrolling
-    whatWheel = 'onwheel' in document.createElement('div') ? 'wheel' : document.onmousewheel !== undefined ? 'mousewheel' : 'DOMMouseScroll';
+    whatWheel =
+      "onwheel" in document.createElement("div")
+        ? "wheel"
+        : document.onmousewheel !== undefined
+        ? "mousewheel"
+        : "DOMMouseScroll";
     window.addEventListener(whatWheel, function (e) {
       var direction = e.wheelDelta || e.deltaY;
       if (direction > 0) {
@@ -22,7 +25,7 @@ var slider = function (sliderElement) {
     });
 
     // allow keyboard input
-    window.addEventListener('keydown', function (e) {
+    window.addEventListener("keydown", function (e) {
       if (keyUp[e.keyCode]) {
         changeSlide(-1);
       } else if (keyDown[e.keyCode]) {
@@ -31,51 +34,69 @@ var slider = function (sliderElement) {
     });
 
     // page change animation is done
-    detectChangeEnd() && document.querySelector(sliderElement).addEventListener(detectChangeEnd(), function () {
-      if (isChanging) {
-        setTimeout(function() {
-          isChanging = false;
-          window.location.hash = document.querySelector('[data-slider-index="' + currentSlide + '"]').id;
-        }, 400);
-      }
-    });
+    detectChangeEnd() &&
+      document
+        .querySelector(sliderElement)
+        .addEventListener(detectChangeEnd(), function () {
+          if (isChanging) {
+            setTimeout(function () {
+              isChanging = false;
+              window.location.hash = document.querySelector(
+                '[data-slider-index="' + currentSlide + '"]'
+              ).id;
+            }, 400);
+          }
+        });
 
     // set up page and build visual indicators
-    document.querySelector(sliderElement).classList.add('slider__container');
-    var indicatorContainer = document.createElement('div');
-    indicatorContainer.classList.add('slider__indicators');
+    document.querySelector(sliderElement).classList.add("slider__container");
+    var indicatorContainer = document.createElement("div");
+    indicatorContainer.classList.add("slider__indicators");
 
     var index = 1;
-    [].forEach.call(document.querySelectorAll(sliderElement + ' > *'), function (section) {
+    [].forEach.call(
+      document.querySelectorAll(sliderElement + " > *"),
+      function (section) {
+        var indicator = document.createElement("a");
+        indicator.classList.add("slider__indicator");
+        indicator.setAttribute("data-slider-target-index", index);
+        indicatorContainer.appendChild(indicator);
 
-      var indicator = document.createElement('a');
-      indicator.classList.add('slider__indicator')
-      indicator.setAttribute('data-slider-target-index', index);
-      indicatorContainer.appendChild(indicator);
-
-      section.classList.add('slider__page');
-      pages.push(section);
-      section.setAttribute('data-slider-index', index++);
-    });
+        section.classList.add("slider__page");
+        pages.push(section);
+        section.setAttribute("data-slider-index", index++);
+      }
+    );
 
     document.body.appendChild(indicatorContainer);
-    document.querySelector('a[data-slider-target-index = "' + currentSlide +'"]').classList.add('slider__indicator--active');
-
+    document
+      .querySelector('a[data-slider-target-index = "' + currentSlide + '"]')
+      .classList.add("slider__indicator--active");
 
     // stuff for touch devices
     var touchStartPos = 0;
     var touchStopPos = 0;
-    var touchMinLength = 90;
-    document.addEventListener('touchstart', function (e) {
+    var touchMinLength = 10;
+    document.addEventListener("touchstart", function (e) {
       e.preventDefault();
-      if (e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel') {
+      if (
+        e.type == "touchstart" ||
+        e.type == "touchmove" ||
+        e.type == "touchend" ||
+        e.type == "touchcancel"
+      ) {
         var touch = e.touches[0] || e.changedTouches[0];
         touchStartPos = touch.pageY;
       }
     });
-    document.addEventListener('touchend', function (e) {
+    document.addEventListener("touchend", function (e) {
       e.preventDefault();
-      if (e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel') {
+      if (
+        e.type == "touchstart" ||
+        e.type == "touchmove" ||
+        e.type == "touchend" ||
+        e.type == "touchcancel"
+      ) {
         var touch = e.touches[0] || e.changedTouches[0];
         touchStopPos = touch.pageY;
       }
@@ -87,26 +108,24 @@ var slider = function (sliderElement) {
     });
   };
 
-
   // prevent double scrolling
   var detectChangeEnd = function () {
     var transition;
-    var e = document.createElement('foobar');
+    var e = document.createElement("foobar");
     var transitions = {
-      'transition': 'transitionend',
-      'OTransition': 'oTransitionEnd',
-      'MozTransition': 'transitionend',
-      'WebkitTransition': 'webkitTransitionEnd'
+      transition: "transitionend",
+      OTransition: "oTransitionEnd",
+      MozTransition: "transitionend",
+      WebkitTransition: "webkitTransitionEnd",
     };
 
-    for(transition in transitions) {
+    for (transition in transitions) {
       if (e.style[transition] !== undefined) {
         return transitions[transition];
       }
     }
     return true;
   };
-
 
   // handle css change
   var changeCss = function (obj, styles) {
@@ -119,9 +138,12 @@ var slider = function (sliderElement) {
 
   // handle page/section change
   var changeSlide = function (direction) {
-
     // already doing it or last/first page, staph plz
-    if (isChanging || (direction == 1 && currentSlide == pages.length) || (direction == -1 && currentSlide == 1)) {
+    if (
+      isChanging ||
+      (direction == 1 && currentSlide == pages.length) ||
+      (direction == -1 && currentSlide == 1)
+    ) {
       return;
     }
 
@@ -129,17 +151,23 @@ var slider = function (sliderElement) {
     currentSlide += direction;
     isChanging = true;
     changeCss(document.querySelector(sliderElement), {
-      transform: 'translate3d(0, ' + -(currentSlide - 1) * 100 + '%, 0)'
+      transform: "translate3d(0, " + -(currentSlide - 1) * 100 + "%, 0)",
     });
 
     // change dots
-    document.querySelector('a.slider__indicator--active').classList.remove('slider__indicator--active');
-    document.querySelector('a[data-slider-target-index="' + currentSlide +'"]').classList.add('slider__indicator--active');
+    document
+      .querySelector("a.slider__indicator--active")
+      .classList.remove("slider__indicator--active");
+    document
+      .querySelector('a[data-slider-target-index="' + currentSlide + '"]')
+      .classList.add("slider__indicator--active");
   };
 
   // go to spesific slide if it exists
   var gotoSlide = function (where) {
-    var target = document.querySelector(where).getAttribute('data-slider-index');
+    var target = document
+      .querySelector(where)
+      .getAttribute("data-slider-index");
     if (target != currentSlide && document.querySelector(where)) {
       changeSlide(target - currentSlide);
     }
@@ -151,17 +179,17 @@ var slider = function (sliderElement) {
       window.scrollTo(0, 0);
       gotoSlide(location.hash);
     }, 1);
-  };
+  }
 
   // we have lift off
-  if (document.readyState === 'complete') {
+  if (document.readyState === "complete") {
     init();
   } else {
-    window.addEventListener('onload', init(), false);
+    window.addEventListener("onload", init(), false);
   }
 
   // expose gotoSlide function
   return {
-    gotoSlide: gotoSlide
-  }
+    gotoSlide: gotoSlide,
+  };
 };
