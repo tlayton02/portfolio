b6271f0457d4ef44455130c2e764aadaccddbbff
tlayton02 <47725456+tomlayton@users.noreply.github.com>
2022-09-04 13:51:44 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+window.onload = function () {
+  const navToggle = document.querySelector(".nav-toggle");
+  const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
+  navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
   });
-});
 
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      document.body.classList.remove("nav-open");
+    });
+  });
+};
 var lastScrollTop; // This Varibale will store the top position
 
 navbar = document.getElementById("navbar"); // Get The NavBar
